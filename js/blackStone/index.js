febb223acddeb2e387fa9ad5f3106702e180aabf
latifbey <latifbey@yandex.com>
2017-10-28 16:13:48 +0200

    langsam langsam

--- js/blackStone/index.js
+++ js/blackStone/index.js
@@ -1,9 +1,10 @@
-$(button).on("click", function(){
+$("button").on("click", function(){
     main();
 })
 
 function main(){
     let satir = $("#row").val();
     let kolon = $("#col").val();
+    console.log(satir,kolon,8);
     return satir,kolon;
 }
