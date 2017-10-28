74de1bd031497e92add5ada6f4080ef67ef80041
latifbey <latifbey@yandex.com>
2017-10-28 17:49:30 +0200

    komm

--- js/blackStone/index.js
+++ js/blackStone/index.js
@@ -1,10 +1,22 @@
-$("button").on("click", function(){
+$("button").on("click", function () {
     main();
 })
 
-function main(){
+function main() {
+    let userInput = getInputValues();
+}
+
+function getInputValues() {
     let satir = $("#row").val();
     let kolon = $("#col").val();
-    console.log(satir,kolon,8);
-    return satir,kolon;
+    console.log(satir, kolon, 8);
+    return {
+        satir,
+        kolon
+    };
 }
+$("section > div > #first").append("<div>.</div>".repeat(satir*kolon));
+
+     
+
+
