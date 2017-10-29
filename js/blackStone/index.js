bdf107e10e04891b6462262b5097e103c3c61e6c
latifbey <latifbey@yandex.com>
2017-10-29 09:34:39 +0100

    olmuyor

--- js/blackStone/index.js
+++ js/blackStone/index.js
@@ -1,3 +1,8 @@
+/**degisken ebatlarda(kullanicinin verdigi bilgilere gore) generik sekilde bir table yapacagiz.
+--oncelikle buttona tiklandiginda kullanicinin girdigi bilgileri tutup alacak bir main function yaziyoruz.
+
+*/
+
 $("button").on("click", function () {
     main();
 })
@@ -9,14 +14,19 @@ function main() {
 function getInputValues() {
     let satir = $("#row").val();
     let kolon = $("#col").val();
-    console.log(satir, kolon, 8);
+    // console.log(satir, kolon, 8);
     return {
         satir,
         kolon
     };
 }
-$("section > div > #first").append("<div>.</div>".repeat(satir*kolon));
+//var parent = $('<div class="barParent">');
 
-     
-
-
+function tableDraw() {
+    $("#maze").html('');
+    let parent = $('<div class="tableparent">').css({'height':'200px','width':'200px','background':'red'});
+    let table = $("<div>.</div>".repeat(satir * kolon));
+    parent.append(table);
+    $("#maze").append(parent);
+    console.log(table, 5);
+}
