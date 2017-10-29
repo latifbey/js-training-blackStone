99f40fd1f6585eaf18d43ddcf212a5941f32fb69
latifbey <latifbey@yandex.com>
2017-10-29 17:16:48 +0100

    olmuyor

--- js/blackStone/index.js
+++ js/blackStone/index.js
@@ -14,19 +14,30 @@ function main() {
 function getInputValues() {
     let satir = $("#row").val();
     let kolon = $("#col").val();
-    // console.log(satir, kolon, 8);
+    console.log(satir, kolon, 8);
     return {
         satir,
         kolon
     };
 }
-//var parent = $('<div class="barParent">');
 
-function tableDraw() {
+
+function tableDraw(userInput) {
     $("#maze").html('');
-    let parent = $('<div class="tableparent">').css({'height':'200px','width':'200px','background':'red'});
-    let table = $("<div>.</div>".repeat(satir * kolon));
-    parent.append(table);
-    $("#maze").append(parent);
-    console.log(table, 5);
+    kolon.forEach(function () {
+        let table = $("<div>.</div>".repeat(parseInt(kolon)));
+        
+        let parent = $('<div class="tableparent">').css({
+            'height': '200px',
+            'width': '200px',
+            'background': 'red'
+        });
+        
+        parent.append(table);
+        $("#maze").append(parent);
+        console.log(table, 5);
+
+    })
+
+
 }
